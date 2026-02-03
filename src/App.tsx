47cffb3a106f13cdbb0c3e3e0f8bed00360c6ba1
muhammadiso0203 <muhammmadiso0203@gmail.com>
2026-02-03 12:42:59 +0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 
 const App = () => {
   return (
-    <div className="w-20 ">ZAYRX WEB</div>
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <div className="text-4xl font-bold">ZAYRX WEB</div>
+    </div>
   )
 }
 
